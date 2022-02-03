4ca62d50cc694424b53950650a99ca13a0fd7e71
trevortrusty <trevortrusty@outlook.com>
2022-02-03 16:52:00 -0500

    Implemented green and yellow color changing when there is a correct letter

--- components/Attempt.js
+++ components/Attempt.js
@@ -5,40 +5,48 @@ import guesses from '../globals/guesses'
 let wordle='whack';
 const Attempt = () => {
 
-    let squareColors = ["red", "white", "white", "white", "white"];
+    let squareColors = ["white", "white", "white", "white", "white"];
 
     const [attempt, setAttempt] = useState('');
     const [color, setColor] = useState(squareColors);
+    const [isSubmit, setIsSubmit] = useState(false);
     const handleAttemptSubmit = (event) => {
+        setIsSubmit(true);
         console.log(attempt);
-        for(let i = 0; i < 5; i++) {
-            if (attempt.charAt(i) == wordle.charAt(i)) {
-                console.log(i.toString() + 'is correct.');
-                color[i] = "green";
-                setColor(color);
-            }
-        }
+        // for(let i = 0; i < 5; i++) {
+        //     if (attempt.charAt(i) == wordle.charAt(i)) {
+        //         console.log(i.toString() + 'is correct.');
+        //         color[i] = "green";
+        //         setColor(color);
+        //     }
+        // }
         event.preventDefault();
     }
     const handleChange = (event) => {
         setAttempt(event.target.value);
     }
+
+    const testClick = () => {
+        console.log("Submit button pressed, test.")
+    }
+
     return(
         <div>
             <div className={styles.attempt}>
-                <div className={styles.squares}>
-                    <Square color={color[0]} place="1" />
-                    <Square color={color[1]} place="2" />
-                    <Square color={color[2]} place="3" />
-                    <Square color={color[3]} place="4" />
-                    <Square color={color[4]} place="5" />
-                </div>
                 <div className={styles.input}>
-                    <form onSubmit={handleAttemptSubmit}>
+                    <form>
                         <input className={styles.inputBox} type="text" name="letter" maxLength="5" spellCheck="false" onChange={handleChange}></input>
-                        <input className={styles.submitBtn} type="submit"></input>
+                        <input className={styles.submitBtn} type="submit" onClick={handleAttemptSubmit}></input>
                     </form>
                 </div>
+                <div className={styles.squares}>
+                    <Square color={color} setColor={setColor} place="0" attempt={attempt} isSubmit={isSubmit} />
+                    <Square color={color} setColor={setColor} place="1" attempt={attempt} isSubmit={isSubmit} />
+                    <Square color={color} setColor={setColor} place="2" attempt={attempt} isSubmit={isSubmit} />
+                    <Square color={color} setColor={setColor} place="3" attempt={attempt} isSubmit={isSubmit} />
+                    <Square color={color} setColor={setColor} place="4" attempt={attempt} isSubmit={isSubmit} />
+                </div>
+                
             </div>
             <div className={styles.spacer}></div>
         </div>
--- components/Square.js
+++ components/Square.js
@@ -1,16 +1,28 @@
 import styles from './Square.module.css';
+let wordle='whack';
 
-
-const Square = ( {color, place} ) => {
+const Square = ( {color, place, attempt, setColor, isSubmit} ) => {
     //color="white";
-    console.log(color);
+    //console.log(color);
+    if(isSubmit)
+    {
+        if (attempt.charAt(place) == wordle.charAt(place)) {
+            console.log(place.toString() + ' is correct.');
+            color[place] = "green";
+            setColor(color);
+        }
+        else if (wordle.includes(attempt.charAt(place))) {
+            console.log(place.toString() + ' is in a different spot')
+            color[place] = "yellow";
+            setColor(color);
+        }
+    }
     return (
-        <div className={styles.square} style={{backgroundColor: {color}}}>
+        <div className={styles.square} style={{backgroundColor: color[place]}}>
             {/* {place} */}
             {/* <form>
                 <input type="text" name="letter" maxLength="1"></input>
             </form> */}
-            
         </div>
     )
 }
